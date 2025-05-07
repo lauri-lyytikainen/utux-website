3092899c0996e34581b53a721bfdda792f05590c
Lauri Lyytikäinen <lauri.lyytikainen1@gmail.com>
2025-05-07 19:35:29 +0300

    fix(FooterClient): adjust layout for footer link groups and ensure proper spacing

--- src/blocks/Accordion/Component.tsx
+++ src/blocks/Accordion/Component.tsx
@@ -4,9 +4,11 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from '@/components/ui/accordion'
+import { Separator } from '@/components/ui/separator'
 import { Accordion as AccordionType } from '@/payload-types'
 import jsxConverters from '@/utilities/richTextConverter'
 import { RichText } from '@payloadcms/richtext-lexical/react'
+import React from 'react'
 
 export function AccordionComponent({ accordions }: AccordionType) {
   return (
@@ -14,18 +16,22 @@ export function AccordionComponent({ accordions }: AccordionType) {
       <Accordion className="w-full">
         {accordions?.map((accordion, index) => {
           return (
-            <AccordionItem key={index}>
-              <AccordionTrigger>
-                <h2>{accordion.title}</h2>
-              </AccordionTrigger>
-              <AccordionContent>
-                {accordion.content && (
-                  <RichText data={accordion.content} converters={jsxConverters} />
-                )}
-              </AccordionContent>
-            </AccordionItem>
+            <React.Fragment key={index}>
+              <Separator />
+              <AccordionItem>
+                <AccordionTrigger>
+                  <h2>{accordion.title}</h2>
+                </AccordionTrigger>
+                <AccordionContent>
+                  {accordion.content && (
+                    <RichText data={accordion.content} converters={jsxConverters} />
+                  )}
+                </AccordionContent>
+              </AccordionItem>
+            </React.Fragment>
           )
         })}
+        {accordions && accordions.length > 0 && <Separator />}
       </Accordion>
     </div>
   )
--- src/components/Footer/FooterClient.tsx
+++ src/components/Footer/FooterClient.tsx
@@ -9,7 +9,7 @@ export function FooterClient({ footerData }: { footerData: Footer }) {
     <footer className="bg-background">
       <div className="max-w-[1024px] mx-auto w-full flex flex-col p-4">
         <div className="flex w-full py-4 gap-4 sm:gap-8 justify-between">
-          <div className="flex flex-col gap-2 pt-2 justify-start grow">
+          <div className="flex flex-col gap-2 pt-2 justify-start grow min-w-16">
             <Image
               src="/media/utux-dark.svg"
               alt="Utux Logo"
@@ -26,20 +26,22 @@ export function FooterClient({ footerData }: { footerData: Footer }) {
             />
             <p className="text-xs font-light">{footerData.slogan}</p>
           </div>
-          {footerData.linkGroups?.map((group) => (
-            <div key={group.id} className="flex flex-col">
-              <h3>{group.title}</h3>
-              {group.links?.map((link) => (
-                <Link
-                  href={(link.linkedPage as Page).breadcrumbs?.at(-1)?.url ?? '/'}
-                  className="hover:underline"
-                  key={link.id}
-                >
-                  {link.text}
-                </Link>
-              ))}
-            </div>
-          ))}
+          <div className="flex flex-row gap-4 flex-wrap">
+            {footerData.linkGroups?.map((group) => (
+              <div key={group.id} className="flex flex-col grow max-w-1/2">
+                <h3>{group.title}</h3>
+                {group.links?.map((link) => (
+                  <Link
+                    href={(link.linkedPage as Page).breadcrumbs?.at(-1)?.url ?? '/'}
+                    className="hover:underline"
+                    key={link.id}
+                  >
+                    {link.text}
+                  </Link>
+                ))}
+              </div>
+            ))}
+          </div>
         </div>
         <Separator />
         <div className="py-4">{footerData.copyrightText}</div>
